87e5c56b094f72a91f16ae190a0fefcbc44c188e
Blessing Sanusi <holuwoleh@gmail.com>
2026-02-06 20:52:42 +0100

    config mongodb and expose env credentials

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV !== 'production') [
-    require('dotenv').load()
+    require('dotenv').config()
 ]
 
 const express = require('express');
@@ -7,6 +7,9 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const webRoutes = require('./routes/web');
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,10 +17,9 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-});
+
+mongoose.connect(process.env.DATABASE_URL);
+
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));

11cd254c9f19be4e06e8dee8fc342c72c78972e7
Blessing Sanusi <holuwoleh@gmail.com>
2026-02-09 23:31:20 +0100

    implement search functionality

--- controllers/AuthorController.js
+++ controllers/AuthorController.js
@@ -2,15 +2,20 @@ const Author = require('../models/Author');
 
 class AuthorController {
     #author;
-    constructor() {
-        this.author = new Author;
-    }
+    constructor() {}
     async index(req, res) {
+        let searchOptions = {};
+        if(req.query.name != null && req.query.name !== '') {
+            searchOptions.name = new RegExp(req.query.name, 'i');
+        }
         try {
-            const authors = await Author.find({});
-            res.render('authors/index', { authors });
+            const authors = await Author.find(searchOptions);
+            res.render('authors/index', { authors,
+                searchOptions: req.query
+            });
         } catch (error) {
             console.log(error);
+            res.redirect('/');
         }
     }
 
@@ -25,25 +30,24 @@ class AuthorController {
         });
     }
 
-    storeAuthor(req, res) {
+    async storeAuthor(req, res) {
         const author = new Author({
             name: req.body.name,
             dob: req.body.dob
         });
-        author.save((err, newAutor) => {
-            if (err) {
-                console.log(err);
-                res.render('authors/create', 
-                    {
-                        author: newAuthor,
-                        errorMessage : 'Error in creating Error'
-                    }
-                );
-            } else {
-                // res.redirect(`authors/${newAuthor._id}`);
-                res.redirect('authors');
-            }
-        });
+        try{
+            await author.save();
+            res.redirect('authors');
+        } catch (error) {
+            console.log(error);
+            res.render('authors/create', 
+                {
+                    author: author,
+                    errorMessage : 'Error in creating Error'
+                }
+            );
+        }
+
     }
  }
 
